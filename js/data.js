1a6fa701e87a8103823470e538625eafade0fcf8
MillyP <millypalmer1@hotmail.co.uk>
2022-02-21 21:25:16 +0000

    Updated data.js with all FAC23 Projects

--- js/data.js
+++ js/data.js
@@ -4,35 +4,141 @@ const data = {
   weeks: [
     {
       week: 1,
-      theme: "Teamwork",
+      theme: "Server",
       projects: [
         {
           title: "Tiny thoughts",
-          img: "",
-          team: ['Paolo,Milly,Juliette,Paolo'],
-          github: 'https://github.com/fac-23/app-week1-team1.git',
-          description: 'Micro-blogging site',
+          img: "assets/tinythoughts.png",
+          team: ["Paolo,Milly,Juliette,Paolo"],
+          github: "https://github.com/fac-23/app-week1-team1.git",
+          description: "Micro-blogging site",
         },
         {
           title: "Moo-net",
           img: "",
-          team: ['Orian,Miah,Oli'],
-          github: 'https://github.com/fac-23/microblogging-oli-orian-miah.git',
+          team: ["Orian,Miah,Oli"],
+          github: "https://github.com/fac-23/microblogging-oli-orian-miah.git",
           description: "Micro-blogging site",
         },
+      ],
+    },
+  ],
+  weeks: [
+    {
+      week: 2,
+      theme: "Database",
+      projects: [
         {
-          title: "Project 3",
+          title: "Who Said That",
           img: "",
-          team: ["Person 1", "Person 2", "Person 3", "Person 4"],
-          github: "https://github.com",
-          description: "Project description",
+          team: ["Miah, Oli, Milly, Adam"],
+          github: "https://github.com/fac-23/week2-database",
+          description: "App that stores data in PostgreSQL database",
         },
         {
-          title: "Project 4",
+          title: "Nom Nom",
           img: "",
-          team: ["Person 1", "Person 2", "Person 3", "Person 4"],
-          github: "https://github.com",
-          description: 'Project description".',
+          team: ["Paolo, Juliette, Orian"],
+          github: "https://github.com/fac-23/week2-database-team2",
+          description: "App that stores data in PostgreSQL database",
+        },
+      ],
+    },
+  ],
+  weeks: [
+    {
+      week: 3,
+      theme: "Authentication",
+      projects: [
+        {
+          title: "Potify",
+          img: "",
+          team: ["Oli, Adam, Paolo"],
+          github: "https://github.com/fac-23/week3-oli-adam-paolo",
+          description:
+            "App that authenticates users and stores data in PostgreSQL database",
+        },
+        {
+          title: "Dev Pop",
+          img: "",
+          team: ["Miah, Orian, Milly, Juliette"],
+          github: "https://github.com/fac-23/week3-auth-team2",
+          description:
+            "App that authenticates users and stores data in PostgreSQL database",
+        },
+      ],
+    },
+  ],
+  weeks: [
+    {
+      week: 4,
+      theme: "Server Side App",
+      projects: [
+        {
+          title: "Dogs and Frogs",
+          img: "",
+          team: ["Oli, Adam, Miah, Juliette"],
+          github: "https://github.com/fac-23/week4_team1",
+          description: "Post pictures of your dogs or frogs",
+        },
+        {
+          title: "Petarazzi",
+          img: "",
+          team: ["Milly, Paolo, Orian"],
+          github: "https://github.com/fac-23/week4-milly-paolo-orian",
+          description: "Post pictures of your pets",
+        },
+      ],
+    },
+  ],
+  weeks: [
+    {
+      week: 5,
+      theme: "Client Side App",
+      projects: [
+        {
+          title: "Zodifac",
+          img: "",
+          team: ["Oli, Milly, Juliette"],
+          github: "https://github.com/fac-23/week5-juliette-oli-milly",
+          description: "Interactive React game using data from an API",
+        },
+        {
+          title: "Fac Quest",
+          img: "",
+          team: ["Paolo, Miah"],
+          github: "https://github.com/fac-23/week5-react-paolo-miah",
+          description: "Interactive React game using data from an API",
+        },
+        {
+          title: "Pokemon Racing Game",
+          img: "",
+          team: ["Adam, Orian"],
+          github: "https://github.com/fac-23/week5-adam-orian",
+          description: "Interactive React game using data from an API",
+        },
+      ],
+    },
+  ],
+  weeks: [
+    {
+      week: 6,
+      theme: "Full Stack App",
+      projects: [
+        {
+          title: "Crocoholics",
+          img: "",
+          team: ["Juliette, Paolo, Miah"],
+          github:
+            "https://github.com/fac-23/week6-fullstack-juliette-paolo-miah",
+          description: "E-commerce Shopping Site",
+        },
+        {
+          title: "Tasty Cakes",
+          img: "",
+          team: ["Adam, Milly, Oli, Orian"],
+          github: "https://github.com/fac-23/week6-adam-milly-oli-orian",
+          description: "E-commerce Shopping Site",
         },
       ],
     },
