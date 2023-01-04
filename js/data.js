08a08220367212aa3c4b51dd493d897195ab8e29
ByteSizedIT <60395899+ByteSizedIT@users.noreply.github.com>
2023-01-04 14:26:45 +0000

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,21 +2,31 @@ console.log("data.js");
 
 const data = {
   weeks: [
-    {
-      week: -2,
-      theme: "Markup - first project of part time course",
-      projects: [],
-    },
-    {
-      week: -1,
-      theme: "HTTP - second project of part time course",
-      projects: [],
-    },
-    {
-      week: 0,
-      theme: "Testing - third project of part time course",
-      projects: [],
-    },
+// Example week object
+//     {
+//       week: 0,
+//       theme: "Tech for Better",
+//       projects: [
+//         {
+//           title: "My Body Back",
+//           img:
+//             "https://camo.githubusercontent.com/aaba1067da6b5933715e09d85a8de74dc36d6a29/68747470733a2f2f692e696d6775722e636f6d2f41474e4e6c74532e706e67",
+//           team: ["Sarah", "Francesca", "Gregor", "Martha"],
+//           github: "https://github.com/fac-17/My-Body-Back",
+//           description:
+//             "The app aims to provide support for people with experience of sexual assault. The app will encourage more people to go for a routince cervical screening (smear test)."
+//         },
+//         {
+//           title: "Autsera",
+//           img:
+//             "https://camo.githubusercontent.com/2a9d11d1232074bd3cee98ef6f3681a6deba0ef0/68747470733a2f2f692e696d6775722e636f6d2f4f5335617179472e706e67",
+//           team: ["Jan", "Reuben", "Leonie", "Victor"],
+//           github: "https://github.com/fac-17/Autsera",
+//           description:
+//             "An interactive game that makes social communication training for children with Autism effective, accessible, affordable and fun."
+//         }
+//       ],
+//     },
     {
       week: 1,
       theme: "Server",
@@ -58,5 +68,19 @@ const data = {
       projects: [],
     },
   ],
+  
+//   Example people array
+//   people: [
+//     {
+//       name: "Reuben",
+//       handle: "reubengt",
+//       img: "https://avatars0.githubusercontent.com/u/42008435"
+//     },
+//     {
+//       name: "Gregor",
+//       handle: "Albadylic",
+//       img: "https://avatars1.githubusercontent.com/u/43413852"
+//     }
+//   ]
   people: [],
 };
